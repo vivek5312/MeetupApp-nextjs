ab9836a1199bec765034b6b7a572c95a350164f7
Vivek Yadav <122459390+vivek5312@users.noreply.github.com>
2023-10-08 16:28:47 +0530

    dynamic routes

--- pages/[meetId]/index.js
+++ pages/[meetId]/index.js
@@ -11,3 +11,43 @@ return(
 )
 
 }
+
+export async function getStaticPaths(){
+    return{
+        fallback:false,
+        paths:[
+        {
+            params:{
+                meetId:'a1',
+            },
+
+        },
+        {
+            params:{
+                meetId:'a2',
+            },
+            
+        },
+        {
+            params:{
+                meetId:'a3',
+            },
+            
+        }
+        ]
+    }
+}
+
+export async function getStaticProps(context){
+    const meetupId=context.params.meetId
+    return{
+        props:{
+            meetupData:{
+            image:'https://upload.wikimedia.org/wikipedia/commons/f/f4/Phyang_Monastery_01.jpg',
+            id:meetupId,
+            title:'A first meetup',
+            desc:'This is first meetup'
+            }
+        }
+    }
+}
--- pages/index.js
+++ pages/index.js
@@ -1,24 +1,45 @@
-import MeetupList from '../components/meetups/MeetupList'
+import MeetupList from '../components/meetups/MeetupList';
 
-const Dummy_Meetup=[{
-    id:1,
-    title:'A first meetup',
-    image:'https://upload.wikimedia.org/wikipedia/commons/f/f4/Phyang_Monastery_01.jpg',
-    des:'This is first meetup'
-},{
-    id:2,
-    title:'A first meetup',
-    image:'https://upload.wikimedia.org/wikipedia/commons/f/f4/Phyang_Monastery_01.jpg',
-    des:'This is first meetup'
+const Dummy_Meetup = [
+  {
+    id:'a1',
+    title: 'A first meetup',
+    image: 'https://upload.wikimedia.org/wikipedia/commons/f/f4/Phyang_Monastery_01.jpg',
+    des: 'This is first meetup',
+  },
+  {
+    id: 'a2',
+    title: 'A first meetup',
+    image: 'https://upload.wikimedia.org/wikipedia/commons/f/f4/Phyang_Monastery_01.jpg',
+    des: 'This is first meetup',
+  },
+];
+
+export default function MeetupPage(props) {
+  return (
+    <div>
+      <MeetupList meetups={props.meetups} />
+    </div>
+  );
 }
-]
-export default function(){
-    return (
-        <div>
-            
-            <MeetupList meetups={Dummy_Meetup}/>
-            
-      
-        </div>
-    )
+export async function getServerSideProps(context){
+ 
+    const req=context.req;
+    const res=context.res;
+
+    return{
+        props:{
+            meetups: Dummy_Meetup,
+        }
+    }
 }
+
+/*export async function getStaticProps() {
+  // Fetch API
+  return {
+    props: {
+      meetups: Dummy_Meetup,
+    },
+  };
+}
+*/
